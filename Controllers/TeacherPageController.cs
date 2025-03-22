4e22c5b2c5a4f003a38dcb613810244d2198b774
Rutul Manani <rutulmanani18@gmail.com>
2025-03-22 13:15:35 -0400

    Update TeacherPageController.cs

--- Controllers/TeacherPageController.cs
+++ Controllers/TeacherPageController.cs
@@ -23,18 +23,5 @@ namespace Cummulative1_schooldb.Controllers
             IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
             return View(Teachers);
         }
-        /// <summary>
-        /// Retrieves detailed information of a specific teacher based on the provided teacher ID.
-        /// </summary>
-        /// <param name="id">The unique identifier of the teacher.</param>
-        /// <returns>A view displaying the teacher's details.</returns>
-        // GET: TeacherPage/Show/{id}
-        public ActionResult Show(int id)
-        {
-            TeacherAPIController controller = new TeacherAPIController();
-            Teacher NewTeacher = controller.FindTeacher(id);
-
-            return View(NewTeacher);
-        }
     }
 }
