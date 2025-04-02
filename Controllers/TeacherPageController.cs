2eec14d01621bb940391922e07ecab4161d6a483
Rutul Manani <rutulmanani18@gmail.com>
2025-04-02 12:55:42 -0400

    Update TeacherPageController.cs

--- Controllers/TeacherPageController.cs
+++ Controllers/TeacherPageController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Cummulative1_schooldb.Models;
@@ -6,22 +6,56 @@ using MySql.Data.MySqlClient;
 
 namespace Cummulative1_schooldb.Controllers
 {
-    /// <summary>
-    /// TeacherPageController handles the actions related to displaying teacher data.
-    /// </summary>
     public class TeacherPageController : Controller
     {
-        /// <summary>
-        /// Retrieves a list of teachers based on the search key provided.
-        /// </summary>
-        /// <param name="SearchKey">The keyword used to filter the teacher list.</param>
-        /// <returns>A view displaying a list of teachers that match the search criteria.</returns>
+        private readonly SchoolDbContext Cummulative1_schooldb = new SchoolDbContext();
+
         // GET: TeacherPage/List
         public ActionResult List(string SearchKey)
         {
             TeacherAPIController controller = new TeacherAPIController();
             IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
             return View(Teachers);
+        }
+
+        // GET: TeacherPage/Show/{id}
+        public ActionResult Show(int id)
+        {
+            if (id <= 0)
+            {
+                return View("Error", new ErrorViewModel { Message = "Invalid Teacher ID" });
+            }
+
+            TeacherAPIController controller = new TeacherAPIController();
+            var result = controller.FindTeacher(id);
+
+            if (result is System.Web.Http.Results.NotFoundResult)
+            {
+                return View("Error", new ErrorViewModel { Message = "Teacher not found" });
+            }
+
+            var okResult = result as System.Web.Http.Results.OkNegotiatedContentResult<Teacher>;
+            if (okResult != null)
+            {
+                return View(okResult.Content);
+            }
+
+            return View("Error", new ErrorViewModel { Message = "An error occurred" });
+        }
+
+        // GET: TeacherPage/SearchByHireDate
+        public ActionResult SearchByHireDate()
+        {
+            return View();
+        }
+
+        // POST: TeacherPage/SearchByHireDate
+        [HttpPost]
+        public ActionResult SearchByHireDate(DateTime minDate, DateTime maxDate)
+        {
+            TeacherAPIController controller = new TeacherAPIController();
+            IEnumerable<Teacher> Teachers = controller.SearchByHireDate(minDate, maxDate);
+            return View("List", Teachers);
         }
     }
 }
