53913b8a175d93d1a9233c5ca5281d528a917e81
Rutul Manani <rutulmanani18@gmail.com>
2025-04-02 12:57:56 -0400

    Create ErrorViewModel.cs

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Cummulative1_schooldb.Models
+{
+    public class ErrorViewModel
+    {
+        public string Message { get; set; }
+    }
+}
